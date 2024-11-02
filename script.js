3d643f111545018c68ba498f0100d41b8af8242a
Zoe Copoulos <zoe.copoulos@gmail.com>
2024-11-02 17:07:31 -0500

    fixed html file so form clearing function should work

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-//onReady
+//onReady function makes sure js is loaded before running functions
 function onReady(){
-
+    console.log('Javascript is working!');
 }
 
 
 // function to add new employee to the table
 
-function addEmployee(){
+// EXAMPLE FROM GROUP ASSIGNMENT FOR REFERENCE, NOT TOTALLY RELEVANT
+//function addEmployee(){
+//     const emojiHome = document.querySelector("#emojiHome")
+//     console.log('emojiHome is: ', emojiHome)
+//      emojiHome.innerHTML += `<span>🔥</span>`
+//   }
 
-}
+function addEmployee(event){
+    const emojiHome = document.querySelector("#emojiHome")
+    console.log('emojiHome is: ', emojiHome)
+     emojiHome.innerHTML += `<span>🔥</span>`
+  }
+
 
 // function to clear form 
 
-function clearForm(){
-
+function submitEmployee(event){
+event.preventDefault()
+console.log("submitEmployee called with event:", event.target)
 }
 
 // function to update total cost
