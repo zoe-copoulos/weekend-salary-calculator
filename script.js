92c0312a5140faf949219e37f9cd99cb183f677d
Zoe Copoulos <zoe.copoulos@gmail.com>
2024-11-02 16:15:39 -0500

    think I messed up moving the index file

---
+++ script.js
@@ -0,0 +1,37 @@
+//onReady
+function onReady(){
+
+}
+
+
+// function to add new employee to the table
+
+function addEmployee(){
+
+}
+
+// function to clear form 
+
+function clearForm(){
+
+}
+
+// function to update total cost
+
+function totalCost(){
+
+}
+
+// function to see if over budget
+
+function checkBudget(){
+
+}
+
+// function to remove an employee from table
+
+function removeEmployee(){
+    
+}
+
+
